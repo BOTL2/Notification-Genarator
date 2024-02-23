463361c277e09da87bf13a2483a80e8aa90c2d36
Anindya Paul <135857817+BOTL2@users.noreply.github.com>
2024-02-23 11:25:01 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+
+        title="PLEASE DRINK WATER ^_^",
+        message="Safe and readily available water is important for public health, fluid intake is about 1.5L daily "
+                "for men .About 11.5 cups for women",
+
+        app_icon="E:\PROJECT NOTIFICATION _101_\icon.ico",
+
+
+        timeout=5
+
+    )
